b141e6761627cb1caa16516c6cc76e74444f45a9
taikhoanhackerrank <124575528+taikhoanhackerrank@users.noreply.github.com>
2023-04-11 16:19:37 +0700

    Update Mystrey number.cpp

--- BT04/Mystrey number.cpp
+++ BT04/Mystrey number.cpp
@@ -1,65 +1,21 @@
-#include <bits/stdc++.h>
-
+#include "bits/stdc++.h"
 using namespace std;
-#define ff(i, a, b) for(int i = a; i <= b; i++)
-#define Rep(i,a, b) for(int i = a; i >= b; i--)
-#define MP make_pair
-typedef long long ll;
-typedef pair<int, int> pi;
-const ll MAXN = 1e2 + 7;
-const ll base = 311;
-const ll INF = 1e9 + 7;
 
-vector<int> A(MAXN), B(MAXN);
+int main(
 
-void Swap(int &x, int &y)
-{
-    int c = y;
-    y = x;
-    x = c;
-}
-
-bool check(int x, int y)
-{
-    if (x <= y) return true;
-    return false;
-}
-
-void sort(int N)
-{
-    ff(i, 1, N - 1)
     {
-        ff(j, i + 1, N) if (!check(A[i], A[j])) swap(A[i], A[j]);
-    }
-
-    ff(i, 1, N)
-    {
-        ff(j, i + 1, N + 1) if (!check(B[i], B[j])) swap(B[i], B[j]);
-    }
-}
-
-void solve()
-{
-    int N; cin >> N;
-    ff(i, 1, N) cin >> A[i];
-    ff(i, 1, N + 1) cin >> B[i];
-    sort(N);
-    ff(i, 1, N)
-    {
-        if (A[i] != B[i])
-        {
-            cout << B[i];
-            return;
-        }
-    }
-    cout << B[N + 1];
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    // freopen("X.inp", "r", stdin);
-    // freopen("Y.out", "w", stdout);
-    solve();
-    return 0;
-}
+        int n;
+        cin >> n;
+        for(int &x:a) cin >>x;
+        for(int &x:b) cin >>x;
+        sort(a, a+n);
+        sort(b, b+1+n);
+         for(int i = 0; i < n; i++){
+             if(a[i]!=b[i]{
+                 cout << b[i];
+                 return 0;
+                }
+            }
+                cout << b[n];
+     }
+           
